0f189cbf27bc7a044526216396268433cd2a5754
pigpigya <28218043a@gmail.com>
2022-06-08 12:50:32 +0800

    card basic

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-var cards = ["1", "2", "3"];
+var cards = ["中立格子", "任意門", "佔領點交換", "前進", "受困山洞",
+ "地圖傳送", "天翻地覆", "大風吹", "失去格子", "守護星", 
+ "山崩", "後退", "拖油瓶", "掉入湖中", "核電廠爆炸", 
+ "獲得格子", "解除狀態", "湖水暴漲"];
 var choose = 0;
 var ranNum = cards.length;
 var str = 0;
@@ -18,7 +21,7 @@ function PickCard() {
     console.log(cards);
     var card = cards[choose];
     var card_div = document.getElementById("card");
-    card_div.innerHTML = `<img src='images/${card}.jpg' alt='Playing Card'>`;
+    card_div.innerHTML = `<img src='事件卡/${card}.jpg' alt='Playing Card'>`;
     choose++;
   }
 
