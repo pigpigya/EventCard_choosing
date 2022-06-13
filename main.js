a9e6066f10470b4a92afbe9268952492c5103e95
pigpigya <28218043a@gmail.com>
2022-06-13 13:51:34 +0800

    card update & detail display

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-var cards = ["中立格子", "任意門", "佔領點交換", "前進", "受困山洞",
- "地圖傳送", "天翻地覆", "大風吹", "失去格子", "守護星", 
- "山崩", "後退", "拖油瓶", "掉入湖中", "核電廠爆炸", 
- "獲得格子", "解除狀態", "湖水暴漲"];
+var cards = ["中立格子1", "任意移動", "後退2", "佔領點交換", "前進1", "受困山洞",
+  "地圖傳送", "天翻地覆", "大風吹", "中立格子2", "失去格子1", "守護星",
+  "山崩", "後退1", "拖油瓶", "獲得格子2", "前進2", "掉入湖中", "核電廠爆炸",
+  "獲得格子1", "解除狀態", "失去格子2", "湖水暴漲"];
 var choose = 0;
 var ranNum = cards.length;
+var card = 0;
 
 cards = getRandom(cards);
 
 function PickCard() {
-  var card_div = document.getElementById("card");
+  var card_div = document.getElementById("pickCard");
   var cardName = document.getElementById("cardName");
   var buttonFont = document.getElementById("randomButton");
 
-  if (choose == cards.length) {    
+  if (choose == cards.length) {
     choose = 0;
     cards = getRandom(cards);
 
@@ -20,14 +21,13 @@ function PickCard() {
     card_div.innerHTML = `<img src='事件卡/back.jpg' alt='Playing Card'>`;
   }
   else {
-    var card = cards[choose];
-    
-    choose++;
-    console.log(cards);
+    card = cards[choose];
 
-    card_div.innerHTML = `<img src='事件卡/${card}.jpg' alt='Playing Card'>`;
+    choose++;
+
+    card_div.innerHTML = `<input type="button" style="background-image:url(事件卡/${card}.jpg);width:278px;height:420px;background-size:contain;" onclick="detail();">`;
     cardName.innerHTML = card;
-    
+
   }
 
   if (choose == cards.length) {
@@ -48,4 +48,17 @@ function getRandom(cards) {
     cards[ran] = cards[cards.length - i - 1];
   }
   return ranCards;
+}
+
+function detail() {
+  var card_div = document.getElementById("pickCard");
+  var card_detail = card + "敘述文字";
+
+  card_div.innerHTML = `<input type="button" style="background-image:url(卡片敘述/${card_detail}.jpg);width:278px;height:420px;background-size:contain;" onclick="toCard()">`;
+}
+
+function toCard() {
+  var card_div = document.getElementById("pickCard");
+  
+  card_div.innerHTML = `<input type="button" style="background-image:url(事件卡/${card}.jpg);width:278px;height:420px;background-size:contain;" onclick="detail();">`;
 }
