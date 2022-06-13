69ea8ffeffeba9a8ff1402cf636a995e5cffc9bc
pigpigya <28218043a@gmail.com>
2022-06-14 00:24:16 +0800

    敘述bug

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var cards = ["中立格子1", "任意移動", "後退2", "佔領點交換", "前進1", "受困山洞",
-  "地圖傳送", "天翻地覆", "大風吹", "中立格子2", "失去格子1", "守護星",
-  "山崩", "後退1", "拖油瓶", "獲得格子2", "前進2", "掉入湖中", "核電廠爆炸",
-  "獲得格子1", "解除狀態", "失去格子2", "湖水暴漲"];
+var cards = ["中立格子1", "任意移動1", "守護星3", "後退2", "佔領點交換1", "前進1", "受困山洞",
+  "地圖傳送1", "前進3", "天翻地覆", "解除狀態2", "大風吹1", "佔領點交換2", "中立格子2", "任意移動2",
+  "失去格子1", "守護星1", "山崩", "後退1", "守護星2", "拖油瓶", "獲得格子2", "前進2", "獲得格子3",
+  "中立格子3", "後退3", "失去格子3", "地圖傳送2", "大風吹2", "掉入湖中", "核電廠爆炸", "獲得格子1",
+  "解除狀態1", "失去格子2", "湖水暴漲"];
 var choose = 0;
 var ranNum = cards.length;
 var card = 0;
@@ -13,6 +14,8 @@ function PickCard() {
   var card_div = document.getElementById("pickCard");
   var cardName = document.getElementById("cardName");
   var buttonFont = document.getElementById("randomButton");
+  var remainCard = document.getElementById("remain");
+  var remain_num = 0;
 
   if (choose == cards.length) {
     choose = 0;
@@ -25,6 +28,8 @@ function PickCard() {
     card = cards[choose];
 
     choose++;
+    console.log(choose);
+    remain_num = "剩餘張數："+(35 - 1);
 
     if (choose == 1) {
       press.innerHTML = "按一下圖片";
@@ -68,6 +73,6 @@ function detail() {
 
 function toCard() {
   var card_div = document.getElementById("pickCard");
-  
+
   card_div.innerHTML = `<input type="button" style="background-image:url(事件卡/${card}.jpg);width:278px;height:420px;background-size:contain;" onclick="detail();">`;
 }
